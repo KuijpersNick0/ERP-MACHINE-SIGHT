688bb09383c642f9c43cb359e6718a45cd334850
KuijpersNick0 <73790831+KuijpersNick0@users.noreply.github.com>
2022-05-03 14:13:46 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -3,9 +3,19 @@ var connection = mysql.createConnection({
     host    :'localhost',
     user    :'root',
     password:'mypass',
-    database:'erp_machinesight2',
-    charset :'utf8mb4'
+    database:'erp_machinesight3',
+    charset :'utf8'
 });
 connection.connect(function(error) {if(error) console.log(error);});
 
+
+// const mariadb = require("mariadb");
+// const connection = mariadb.createPool({
+    //      host: 'mydb.com', 
+    //      user:'myUser', 
+    //      password: 'myPassword',
+    //      connectionLimit: 25
+    // });
+//connection.connect(function(error) {if(error) console.log(error);});
+    
 module.exports = connection; 
